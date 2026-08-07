ebdf7668fc090801a889dfb3fe5a8d8afab3139b
Mayuresh Jagtap <mayureshjagu1306@gmail.com>
2026-08-07 23:32:49 +0530

    Remove node_modules and dist from git, add .gitignore and .nojekyll

--- backend/dist/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const admin_controller_1 = require("../controllers/admin.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.post('/login', admin_controller_1.login);
-router.get('/stats', auth_middleware_1.authenticateAdmin, admin_controller_1.getDashboardStats);
-exports.default = router;
--- backend/dist/routes/domain.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const domain_controller_1 = require("../controllers/domain.controller");
-const router = (0, express_1.Router)();
-router.get('/', domain_controller_1.getDomains);
-exports.default = router;
